4b3866b1206ce7689bf8076544b3c30c481ce782
Sachin20010517 <sachinayeshmantha"@gmail.com>
2024-01-08 17:50:46 +0530

    Just tried.But then table didn'twork

--- src/User.java
+++ src/User.java
@@ -1,8 +1,11 @@
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User{
     private String userName;
     private String password;
+    private List<Product> purchaseHistory;
+    private boolean isFirstPurchase;
 
     public User(){
 
@@ -11,6 +14,9 @@ public class User{
     public User(String userName, String password) {
         this.userName = userName;
         this.password = password;
+        this.purchaseHistory = new ArrayList<>();
+        this.isFirstPurchase = true;
+
     }
 
     public String getUserName() {
@@ -28,4 +34,30 @@ public class User{
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Product> getPurchaseHistory() {
+        return purchaseHistory;
+    }
+    public boolean isFirstPurchase() {
+        return isFirstPurchase;
+    }
+
+    public void setFirstPurchase(boolean isFirstPurchase) {
+        this.isFirstPurchase = isFirstPurchase;
+    }
+    public void addPurchaseToHistory(Product product) {
+        purchaseHistory.add(product);
+    }
+    public String getPurchaseHistoryAsString() {
+        StringBuilder purchaseHistoryString = new StringBuilder();
+        for (Product product : purchaseHistory) {
+            purchaseHistoryString.append(product.getProductId()).append(",");
+        }
+        return purchaseHistoryString.toString();
+    }
+    public void addProductToPurchaseHistory(Product product) {
+        purchaseHistory.add(product);
+    }
+
+
 }
